2513ac215d56cd85029a713acf6b7f62ff0b3de7
liumapp <liumapp.com@gmail.com>
2018-04-20 10:46:56 +0800

    add img & style

--- src/main.js
+++ src/main.js
@@ -1,8 +1,19 @@
+/**
+ * @file main.js
+ * @author liumapp
+ * @email liumapp.com@gmail.com
+ * @homepage http://www.liumapp.com
+ * @date 4/20/18
+ */
+
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import iView from 'iview'
+
+Vue.use(iView)
 
 Vue.config.productionTip = false
 
